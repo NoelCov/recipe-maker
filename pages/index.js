2b96b9136a0e2add10fc44ef6fc046a1215d6f46
NoelCov <73734505+NoelCov@users.noreply.github.com>
2021-10-20 14:20:50 -0700

    Modified homepage styling, created recipe component

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,43 @@
 import styles from "../styles/Home.module.scss";
 
 import Button from "../components/button/button.component";
+import Recipe from "../components/recipe/recipe.component";
 
 export default function Home() {
   return (
+    <>
     <div className={styles.homeContainer}>
       <h1 className={styles.title}>Mediterranean Recipe Builder</h1>
       <div className={styles.buttonsContainer}>
         <Button>Create Recipe</Button>
         <Button>Random Recipe</Button>
       </div>
-      <footer>
 
-      </footer>
+      <section className={styles.recipesContainer}>
+        <Recipe
+          title="Chicken Broth"
+          ingredients={"Potatoes, Chicken, Salt"}
+          link="https://www.noelcodes.dev/"
+        />
+
+        <Recipe
+          title="Chicken Broth"
+          ingredients={"Potatoes, Chicken, Salt"}
+          link="https://www.noelcodes.dev/"
+        />
+
+        <Recipe
+          title="Chicken Broth"
+          ingredients={"Potatoes, Chicken, Salt"}
+          link="https://www.noelcodes.dev/"
+        />
+      </section>
     </div>
+    <footer className={styles.footer}>
+        <span>
+          Created by <a href="https://www.noelcodes.dev/">Noel Codes</a>
+        </span>
+      </footer>
+    </>
   );
 }
